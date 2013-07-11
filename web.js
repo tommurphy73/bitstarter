0a46eb2dcd8b2e6b0e6d850a8631022bd19e194b
tommurphy73 <tommurphy73@gmail.com>
2013-07-11 08:51:12 +0000

    Added updated file

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer ('Test');
+
+fs.readFile('./index.html', function (err, data) {
+  if (err) throw err;
+  buf = buf.toString(data);
+   
+});
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('buf');
 });
 
 var port = process.env.PORT || 5000;
